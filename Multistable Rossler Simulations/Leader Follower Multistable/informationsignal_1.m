b0c2c491a992c7b207a2e6f69d43d751716073fd
leslie-swe <ingeniera.jordan@gmail.com>
2025-11-18 16:32:42 -0600

    Mi commit

---
+++ Multistable Rossler Simulations/Leader Follower Multistable/informationsignal_1.m
@@ -0,0 +1,38 @@
+function signal_waveform = informationsignal_1(N)
+% --- 1. Define Simulation Parameters ---
+%N = 970;            % Number of bits to simulate
+Rb = 1000;         % Bit rate (bits per second)
+Tb = 1/Rb;         % Bit duration (time for one bit)
+
+% --- 2. Generate the Random Bits (The Information) ---
+% Use randi to get 0s and 1s
+bits = randi([0, 1], 1, N);
+
+% --- 3. Create the Signal Waveform ---
+% We need to make the plot clear. Let's use 100 samples
+% to represent each bit in the plot.
+samples_per_bit = 10;
+
+
+% 'repelem' repeats each element. This "stretches" each bit
+% to last for 100 samples.
+% Example: [0 1] becomes [0 0...0 1 1...1]
+signal_waveform = repelem(bits, samples_per_bit);
+
+% --- 4. Create the Time Vector ---
+% The total sampling frequency (samples per second)
+Fs = Rb * samples_per_bit; 
+total_samples = N * samples_per_bit;
+
+% Create a time vector 't' that goes from 0 up to
+% the total time, with one time point for each sample.
+t = (0 : total_samples - 1) / Fs;% 100000/50
+
+% --- 5. Plot the Result ---
+% plot(t, signal_waveform, 'LineWidth', 2); % Plot with a thick line
+% grid on;
+% ylim([-0.5, 1.5]);  % Set Y-axis limits to see 0 and 1 clearly
+% xlabel('Time (s)');
+% ylabel('Amplitude (Voltage)');
+% title('Simulated Digital Information Signal (NRZ)');
+% save signal_waveform.dat signal_waveform -ascii
--- Multistable Rossler Simulations/Multistable Rossler Only Leader/solucionrosslermultiestable.m
+++ Multistable Rossler Simulations/Multistable Rossler Only Leader/solucionrosslermultiestable.m
@@ -1,14 +1,29 @@
 clear all 
 clc
+r = 4;           % parámetro de control
+x0 = 0.7;        % condición inicial (entre 0 y 1)
+N = 300;         % número de iteraciones
+%for i=1:1:5
+%    x0= 0.1+0.17*i;
+
+% Inicialización del vector
+y01 = zeros(N, 1);
+y01(1) = x0;
+
+% Iteración del mapa logístico
+for n = 1:N-1
+    y01(n+1) = r * y01(n) * (1 - y01(n));
+end
 
 for j=1:1:100
     
-    y01=-8+0.16*(j-1);
-    Za=[-0.1,0.01,0.3,y01,0.2,0.2];% Iterate the initial condition to analyze how the master is affected
+    % y01=-8+0.16*(j-1);
+    Za=[-0.1,0.01,0.3,y01(j),0.2,0.2];% Iterate the initial condition to analyze how the master is affected
    
 tini=1;tfinal=1000;M=10000;
 [T, Z] =rks4('maestro',tini,tfinal,Za,M);%  This function solves the system.
 % The solution of each state variable is saved in matrix Z.
+
 x=Z(:,1); 
 y=Z(:,2);
 z=Z(:,3);
@@ -18,7 +33,7 @@ z2=Z(:,6);
 
 figure(1); % Create a new figure
 plot3(x, y, z);% Plot the attractor for each value of the initial condition Y01.
-title([ ',j = ',num2str(j),';y01=',num2str(y01)])
+title([ ',j = ',num2str(j),';y01=',num2str(y01(j))])
 
 xlabel('X Axis');
 ylabel('Y Axis');
@@ -27,7 +42,7 @@ grid;
 
 figure(2);% Plot the second attractor of the system as we can't plot the 6 state variables in the same figure
 plot3(x2, y2, z2);
-title([ ',j = ',num2str(j),';y01=',num2str(y01)])
+title([ ',j = ',num2str(j),';y01=',num2str(y01(j))])
 
 xlabel('X Axis');
 ylabel('Y Axis');
@@ -39,7 +54,8 @@ pause(0.5);
 x2_sol(:,j)=Z(:,4);
 
 end
+
 % Saving the time series of the state variable to plot the bifurcation diagram later
- save xx2.dat x2_sol -ascii
- save tplot.dat T   -ascii
+ % save xx2.dat x2_sol -ascii
+ % save tplot.dat T   -ascii
   
